86e63b03e1be582f09d96f6ee93db7c8d569e04c
Junanjunan <taltalrealty@gmail.com>
2022-10-24 23:21:36 +0900

    DRF user serializer  setting example

--- users/serializers.py
+++ users/serializers.py
@@ -1,2 +1,26 @@
 from rest_framework import serializers
 from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        
+        fields = (
+            'id',
+            'username',
+            'password',
+        )
+
+        read_only_fields = (
+            'id',
+        )
+
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        user = user().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
